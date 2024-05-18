a62c81c6a7cb0151dbd166f5a1e222c0301f61cf
Alina Kulish <kylish.alina.dp@gmail.com>
2024-05-18 13:30:31 +0300

    switch to Id and prefilled positions

--- src/app/components/category/category.component.config.ts
+++ src/app/components/category/category.component.config.ts
@@ -1,19 +1,18 @@
 export interface QuestionItem {
-  position: number;
+  id: number;
   question: string;
-  actions: string[];
   answer?: string;
 }
 
 export const MOCK_DATA: QuestionItem[] = [
-  {position: 1, question: 'Can you explain what a component was in Angular?', actions: []},
-  {position: 2, question: 'How do you bind data in Angular?', actions: []},
-  {position: 3, question: 'Can you explain the lifecycle hooks in Angular?', actions: []},
-  {position: 4, question: 'Can you explain the concept of dynamic components in Angular?', actions: []},
-  {position: 5, question: 'What are the best practices for structuring a large Angular application?', actions: []},
-  {position: 6, question: 'What is a directive in Angular? Can you give an example?', actions: []},
-  {position: 7, question: 'What is the difference between an attribute directive and a structural directive?', actions: []},
-  {position: 8, question: 'How do you create custom directives in Angular?', actions: []},
-  {position: 9, question: 'Can you explain the concept of Angular decorators like @HostListener and @HostB?', actions: []},
-  {position: 10, question: 'What is the purpose of the Angular Render?', actions: []},
+  {id: 1, question: 'Can you explain what a component was in Angular?'},
+  {id: 2, question: 'How do you bind data in Angular?'},
+  {id: 3, question: 'Can you explain the lifecycle hooks in Angular?'},
+  {id: 4, question: 'Can you explain the concept of dynamic components in Angular?'},
+  {id: 5, question: 'What are the best practices for structuring a large Angular application?'},
+  {id: 6, question: 'What is a directive in Angular? Can you give an example?'},
+  {id: 7, question: 'What is the difference between an attribute directive and a structural directive?'},
+  {id: 8, question: 'How do you create custom directives in Angular?'},
+  {id: 9, question: 'Can you explain the concept of Angular decorators like @HostListener and @HostB?'},
+  {id: 10, question: 'What is the purpose of the Angular Render?'},
 ];
--- src/app/components/preparation/preparation.component.config.ts
+++ src/app/components/preparation/preparation.component.config.ts
@@ -1,14 +1,14 @@
 import { QuestionItem } from "../category/category.component.config";
 
 export const MOCK_DATA: QuestionItem[] = [
-  {position: 1, question: 'Can you explain what a component was in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 2, question: 'How do you bind data in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 3, question: 'Can you explain the lifecycle hooks in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 4, question: 'Can you explain the concept of dynamic components in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 5, question: 'What are the best practices for structuring a large Angular application?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 6, question: 'What is a directive in Angular? Can you give an example?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 7, question: 'What is the difference between an attribute directive and a structural directive?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 8, question: 'How do you create custom directives in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 9, question: 'Can you explain the concept of Angular decorators like @HostListener and @HostB?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
-  {position: 10, question: 'What is the purpose of the Angular Render?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.', actions: []},
+  {id: 1, question: 'Can you explain what a component was in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 2, question: 'How do you bind data in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 3, question: 'Can you explain the lifecycle hooks in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 4, question: 'Can you explain the concept of dynamic components in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 5, question: 'What are the best practices for structuring a large Angular application?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 6, question: 'What is a directive in Angular? Can you give an example?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 7, question: 'What is the difference between an attribute directive and a structural directive?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 8, question: 'How do you create custom directives in Angular?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 9, question: 'Can you explain the concept of Angular decorators like @HostListener and @HostB?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
+  {id: 10, question: 'What is the purpose of the Angular Render?', answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis lacus sem. Mauris bibendum, ante id dapibus mollis.'},
 ];
