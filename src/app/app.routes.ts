921e1938a077449f30b0ef981b9dc5d09b1e0b4c
Alina Kulish <kylish.alina.dp@gmail.com>
2024-05-26 18:40:02 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
 import { PreparationComponent } from './components/preparation/preparation.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -10,9 +9,3 @@ export const routes: Routes = [
   { path: '', redirectTo: '/categories/angular', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
